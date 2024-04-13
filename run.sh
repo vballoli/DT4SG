7c7eaa5829e1258ef0ee9c284b6655ae8d15a002
Marc Brooks <marcbr@stat-marcbr-mba.local>
2024-04-13 14:32:36 -0400

    Adding configuration for random policy

--- run.sh
+++ run.sh
@@ -4,7 +4,14 @@
 # bash run_gl.sh run_sac.sh sac_1 1
 # bash run_gl.sh run_sac.sh sac_2 2
 
-bash run_gl.sh run_td3.sh td3_0 0
-bash run_gl.sh run_td3.sh td3_1 1
-bash run_gl.sh run_td3.sh td3_2 2
+# bash run_gl.sh run_td3.sh td3_0 0
+# bash run_gl.sh run_td3.sh td3_1 1
+# bash run_gl.sh run_td3.sh td3_2 2
 
+bash run_gl.sh run_discrete_random.sh discrete_random_0 0
+# bash run_gl.sh run_discrete_random.sh discrete_random_1 1
+# bash run_gl.sh run_discrete_random.sh discrete_random_2 2
+
+bash run_gl.sh run_random.sh random_0 0
+# bash run_gl.sh run_random.sh random_1 1
+# bash run_gl.sh run_random.sh random_2 2
---
+++ run_discrete_random.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python train_online.py algo_config='discrete_random' ++seed=$1
