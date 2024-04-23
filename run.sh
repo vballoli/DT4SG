9d988feea59135713a29397940361f6990b19267
Marc Brooks <mgbrooks208@gmail.com>
2024-04-23 00:12:23 -0400

    running pid / bb policies

--- run.sh
+++ run.sh
@@ -8,10 +8,23 @@
 # bash run_gl.sh run_td3.sh td3_1 1
 # bash run_gl.sh run_td3.sh td3_2 2
 
-bash run_gl.sh run_discrete_random.sh discrete_random_0 0
-bash run_gl.sh run_discrete_random.sh discrete_random_1 1
-bash run_gl.sh run_discrete_random.sh discrete_random_2 2
+# bash run_gl.sh run_discrete_random.sh discrete_random_0 0
+# bash run_gl.sh run_discrete_random.sh discrete_random_1 1
+# bash run_gl.sh run_discrete_random.sh discrete_random_2 2
 
-bash run_gl.sh run_random.sh random_0 0
-bash run_gl.sh run_random.sh random_1 1
-bash run_gl.sh run_random.sh random_2 2
+# bash run_gl.sh run_random.sh random_0 0
+# bash run_gl.sh run_random.sh random_1 1
+# bash run_gl.sh run_random.sh random_2 2
+
+
+# bash run_gl.sh run_pid_expert.sh pid_expert_0 0
+# bash run_gl.sh run_pid_expert.sh pid_expert_1 1
+# bash run_gl.sh run_pid_expert.sh pid_expert_2 2
+
+bash run_gl.sh run_pid_base.sh pid_base_0 0
+bash run_gl.sh run_pid_base.sh pid_base_1 1
+bash run_gl.sh run_pid_base.sh pid_base_2 2
+
+# bash run_gl.sh run_bb.sh bb_0 0
+# bash run_gl.sh run_bb.sh bb_1 1
+# bash run_gl.sh run_bb.sh bb_2 2
